52c8a67b9650fb5f8338ff56f80a9a8fc89d458a
anton.musiienko <anton.musiienko@funambol.com>
2017-07-11 17:00:37 +0300

    [REFACTORING] YouTubeAPI was refactored, magic numbers moved to constants

--- app/src/main/java/ua/com/free/localmusic/youtube/YoutubeAPI.java
+++ app/src/main/java/ua/com/free/localmusic/youtube/YoutubeAPI.java
@@ -41,29 +41,38 @@ import java.util.List;
 public class YoutubeAPI {
 
     private static final String TAG = "YoutubeAPI";
+    private static final String YOUTUBE_API_KEY = "AIzaSyBEmcUz19tz4R_HnV6aqMs-2VRBGNbQLK4";
+    private static final String SEARCH_PARTS = "id,snippet";
+    private static final String SEARCH_QUERY = "items(id/videoId,snippet/title,snippet/thumbnails/default/url)";
+    private static final String SEARCH_TYPE = "video";
+    private static final String HEADER_NAME_PACKAGE = "X-Android-Package";
+    private static final String HEADER_NAME_CERTIFICATE = "X-Android-Cert";
+    private static final long SEARCH_MAX_RESULT = 10L;
+    private static final String ALGORITHM_TYPE = "SHA-1";
 
-    public void search(final Context context) {
+    public void search(final Context context, String searchQuery) {
         YouTube youTube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), new HttpRequestInitializer() {
+
             @Override
             public void initialize(HttpRequest request) throws IOException {
                 String packageName = context.getPackageName();
                 String SHA1 = getSHA1(context, packageName);
 
-                request.getHeaders().set("X-Android-Package", packageName);
-                request.getHeaders().set("X-Android-Cert",SHA1);
+                request.getHeaders().set(HEADER_NAME_PACKAGE, packageName);
+                request.getHeaders().set(HEADER_NAME_CERTIFICATE, SHA1);
             }
         }).setApplicationName(context.getApplicationInfo().packageName).build();
 
         try {
-            YouTube.Search.List search = youTube.search().list("id,snippet");
-            search.setKey("AIzaSyBEmcUz19tz4R_HnV6aqMs-2VRBGNbQLK4");
-            search.setQ("Bring me the horizon");
-            search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
-            search.setMaxResults(15L);
+            YouTube.Search.List search = youTube.search().list(SEARCH_PARTS);
+            search.setKey(YOUTUBE_API_KEY);
+            search.setQ(searchQuery);
+            search.setFields(SEARCH_QUERY);
+            search.setMaxResults(SEARCH_MAX_RESULT);
 
             // Restrict the search results to only include videos. See:
             // https://developers.google.com/youtube/v3/docs/search/list#type
-            search.setType("video");
+            search.setType(SEARCH_TYPE);
 
             SearchListResponse searchResponse = search.execute();
             List<SearchResult> searchResultList = searchResponse.getItems();
@@ -73,15 +82,14 @@ public class YoutubeAPI {
         }
     }
 
-    private String getSHA1(Context context, String packageName){
+    private String getSHA1(Context context, String packageName) {
         try {
             Signature[] signatures = context.getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES).signatures;
-            for (Signature signature: signatures) {
-                MessageDigest md;
-                md = MessageDigest.getInstance("SHA-1");
-                md.update(signature.toByteArray());
-                return BaseEncoding.base16().encode(md.digest());
-            }
+            Signature signature = signatures[0];
+            MessageDigest md;
+            md = MessageDigest.getInstance(ALGORITHM_TYPE);
+            md.update(signature.toByteArray());
+            return BaseEncoding.base16().encode(md.digest());
         } catch (PackageManager.NameNotFoundException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
